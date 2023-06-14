4169b5414fccccf6b9923cb3d02b1d8aeb1c3e7d
WandaSimeoli <wandasimeoli@gmail.com>
2023-06-14 16:00:11 +0200

    Inizializzazione progetto

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const clickButton = document-getElementById('click-button');
+console.log(clickButton, typeof clickButton);
+
+clickButton.addEventListener('click', 
+function() {
+
+const km = parseInt(document.getElementById('km').value);
+console.log(km, typeof km);
+
+const age = parseInt(document.getElementById('age').value);
+console.log(age, typeof age);
+
+let prezzoBiglietto = km * 0.21;
+console.log(prezzoBiglietto, typeof prezzoBiglietto);
+
+if (age < 18) {
+      x = prezzoBiglietto * 0.8;
+     } else if (age >= 65) {
+        x = prezzoBiglietto * 0.6; 
+     } else {
+        x = prezzoBiglietto;
+     }
+}
+);
+
+// console.log(km, typeof km);
+// console.log(age, typeof age);
+
+// if (age < 18) {
+//   x = prezzoBiglietto * 0.8;
+//  } else if (age >= 65) {
+//     x = prezzoBiglietto * 0.6; 
+//  } else {
+//     x = prezzoBiglietto;
+//  }
+// console.log(x, typeof x);
+
+// const myButton = document.getElementById('click-button');
+
+// myButton.addEventListener('click', 
+//     function() {
+//         const kmInput = document.querySelector('[name="km"]');
+//         console.log(kmInput.value);
+//          const ageInput = document.querySelector('[name="age"]');
+//          console.log(ageInput.value);
+         
+//     }
+// );
+// document.querySelector('h1').innerHTML
+
+// document.querySelector('#my_id').innerHTML = x.toFixed(2)
+
+// if(isNaN(age) || isNaN(km)){
+//     alert('Il valore inserito non è valido!')
+//  } 
